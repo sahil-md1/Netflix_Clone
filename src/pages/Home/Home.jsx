df2a1f60623ac1f91c3bd704213256076ea94351
md sahil <ms.mdsahil77@gmail.com>
2025-03-30 00:49:48 +0530

    hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
-
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_titile from "../../assets/hero_title.png";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} className="banner-img"></img>
+        <div className="hero-caption">
+          <img src={hero_titile} className="caption-img"></img>
+          <p>
+            The Predator is a 2018 American science fiction action film directed
+            by Shane Black, who co-wrote the script with Fred Dekker. It is the
+            fourth installment in the Predator franchise, but counting the Alien
+            vs. Predator
+          </p>
+          <div className="hero-btns"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

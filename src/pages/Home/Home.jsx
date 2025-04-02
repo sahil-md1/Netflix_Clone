9c2f8d4b74ea130dd2cc21243ede55f73b3eb100
md sahil <ms.mdsahil77@gmail.com>
2025-04-03 00:40:15 +0530

    deployment

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner3.jpg";
 import hero_titile from "../../assets/hero_title.png";
-import play_icon from "../../assets/Play_icon.png";
+import Play_icon from "../../assets/Info_icon.png";
 import info_icon from "../../assets/Info_icon.png";
 import TitleCards from "../../components/TitleCards/TitleCards";
 import Footer from "../../components/Footer/Footer";
@@ -26,7 +26,7 @@ const Home = () => {
           <div className="hero-btns">
             <button className="btn">
               {" "}
-              <img src={play_icon} className=""></img>Play
+              <img src={Play_icon} className=""></img>Play
             </button>
             <button className="btn dark-btn">
               {" "}

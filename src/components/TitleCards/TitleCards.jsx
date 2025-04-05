c6bf83912d428a8e5efeadfcabae8626ab3b52be
md sahil <ms.mdsahil77@gmail.com>
2025-04-05 12:40:17 +0530

    deployement

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,58 +2,77 @@ import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Skeleton from "react-loading-skeleton";
+import spinner from "../../assets/spinner5.gif";
 
 const TitleCards = ({ name, category }) => {
   const [movieData, setMovieData] = useState([]);
-  // const [loadingState, setLoadingState] = useState([]);
+  const [loadingState, setLoadingState] = useState(true);
 
   const cardRefs = useRef();
-
-  // const url = `https://tvshow.p.rapidapi.com/Movie/${category? category: 'NowPlaying'}?Page=1&Language=en-US&Adult=true`;
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     'x-rapidapi-key': 'fd39ff7006msh93cb556bcde6227p16e0b1jsn787ae99114a9',
-  //     'x-rapidapi-host': 'tvshow.p.rapidapi.com'
-  //   }
-  // };
 
   const url = `https://tvshow.p.rapidapi.com/Movie/${category? category: 'NowPlaying'}?Page=1&Language=en-US&Adult=true`;
   const options = {
     method: 'GET',
     headers: {
-      'x-rapidapi-key': '58d1990c26mshea4927b1ab7ed57p1afc7ajsn3ff192085637',
+      'x-rapidapi-key': 'fd39ff7006msh93cb556bcde6227p16e0b1jsn787ae99114a9',
       'x-rapidapi-host': 'tvshow.p.rapidapi.com'
     }
   };
 
+  // const url = `https://tvshow.p.rapidapi.com/Movie/${
+  //   category ? category : "NowPlaying"
+  // }?Page=1&Language=en-US&Adult=true`;
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "x-rapidapi-key": "58d1990c26mshea4927b1ab7ed57p1afc7ajsn3ff192085637",
+  //     "x-rapidapi-host": "tvshow.p.rapidapi.com",
+  //   },
+  // };
+
   useEffect(() => {
+    setLoadingState(true);
     fetch(url, options)
       .then((response) => response.json())
       .then((result) => {
         if (Array.isArray(result)) {
           setMovieData(result);
         }
+        setLoadingState(false);
       })
+      // setLoadingState(false)
       .catch((error) => console.error(error, "Error fetchimf data:"));
   }, []);
 
-  console.log(movieData, "type");
+  // console.log(movieData, "type");
 
   return (
     <div className="title-cards">
       <h2> {name ? name : "Popular on Netflix"}</h2>
-      <div className="card-list" ref={cardRefs}>
-        {movieData.map((data, idx) => {
-        {/* {cards_data.map((data, idx) => { */}
-          return (
-            <Link to={`/Player/${data.id}`} className="card" key={idx}>
-              <img src={data.image} alt={data.title}></img>
-              <p>{data.originalTitle}</p>
-            </Link>
-          );
-        })}
-      </div>
+      {loadingState ? (
+        <div className="loading">Loading</div>
+      ) : (
+        <div className="card-list" ref={cardRefs}>
+          {movieData.map((data, idx) => {
+            {
+              /* {cards_data.map((data, idx) => { */
+            }
+            return (
+              <Link to={`/Player/${data.id}`} className="card" key={idx}>
+                <LazyLoadImage
+                  src={data.image}
+                  alt={data.title}
+                  effect="blur"
+                />
+                {/* <img src={data.image} alt={data.title}></img> */}
+                <p>{data.originalTitle}</p>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -15,14 +15,14 @@ const Player = () => {
   const url = `https://tvshow.p.rapidapi.com/Video/${id}/Movie?Language=en-US`;
   const options = {
     method: "GET",
-    headers: {
-      'x-rapidapi-key': '58d1990c26mshea4927b1ab7ed57p1afc7ajsn3ff192085637',
-      'x-rapidapi-host': 'tvshow.p.rapidapi.com'
-    }
     // headers: {
-    //   "x-rapidapi-key": "fd39ff7006msh93cb556bcde6227p16e0b1jsn787ae99114a9",
-    //   "x-rapidapi-host": "tvshow.p.rapidapi.com",
-    // },
+    //   'x-rapidapi-key': '58d1990c26mshea4927b1ab7ed57p1afc7ajsn3ff192085637',
+    //   'x-rapidapi-host': 'tvshow.p.rapidapi.com'
+    // }
+    headers: {
+      "x-rapidapi-key": "fd39ff7006msh93cb556bcde6227p16e0b1jsn787ae99114a9",
+      "x-rapidapi-host": "tvshow.p.rapidapi.com",
+    },
   };
 
   useEffect(() => {

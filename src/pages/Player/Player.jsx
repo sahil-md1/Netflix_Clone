8142ce32ee0153b59f61a451c3efd74af697e930
md sahil <ms.mdsahil77@gmail.com>
2025-04-18 22:50:24 +0530

    changes

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -45,7 +45,8 @@ const Player = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  // console.log(movieData, "type"); 
+  console.log(movieData, "type"); 
+console.log('data');
 
   return (
     <div className="player">
